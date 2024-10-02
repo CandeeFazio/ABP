46f2b605965159d9b17525dba85d31899fd3e06f
CandeeFazio <cfaziodening@gmail.com>
2024-10-02 15:05:08 -0300

    first commit

--- funciones.js
+++ funciones.js
@@ -6,66 +6,44 @@ const like = () => {
     document.getElementById("corazon").style.color="red";
 }
 
+const fotos = [
+    { usuario: "Candelitttaaa", imgusuario: "usuario.webp", imagen: "messi.jpg", megusta: 10, comentario: "", },
+    { usuario: "Vickkkk", imgusuario: "usuario.webp", imagen: "messi2.jpg", megusta: 10, comentario: "", },
+    { usuario: "Laritaa", imgusuario: "usuario.webp", imagen: "messi.jpg", megusta: 11, comentario: "", },
+]
 
-const registrar = () => {
-    let contenidoboton = document.getElementById("btnregistrar").innerHTML
-    if (contenidoboton == "Registrar") {
-        document.getElementById("boxconfircontrasena").style.display = "flex";
-        document.getElementById("btnlogin").style.display = "none";
-        document.getElementById("btnregistrar").innerHTML = "Crear nuevo usuario"
-    } else {
-        contrasena = document.getElementById("inputcontrasena").value;
-        confircontrasena = document.getElementById("inputconfircontrasena").value
-        if (contrasena == confircontrasena){
-            let usuario = document.getElementById("inputusuario").value;
-            let contrasena = document.getElementById("inputcontrasena").value;
-            localStorage.setItem(usuario, contrasena)
-            alert("Se ha creado un nuevo usuario")
-        }else{
-            alert("contraseñas incorrectas")
-        }
+function cargarfotos(){
+    for(const foto of fotos) {
+    let contenedor = document.createElement("div")
+    contenedor.innerHTML=` <div id="boxfoto">
+                                <div id="boxusuario">
+                                    <img src=${fotos.imgusuario} alt="" id="imgusuario">
+                                    <h2>${fotos.usuario}</h2>
+                                </div>
+                                <img src=${foto.imagen} alt="foto de paisaje" id="imgfoto">
+                                <div id="boxbotones">
+                                    <button onclick="like()">
+                                        <span class="material-symbols-outlined" id="corazon">
+                                        favorite
+                                        </span>
+                                    </button>
+                                    <button>
+                                        <span class="material-symbols-outlined">
+                                            chat_bubble
+                                        </span>
+                                    </button>
+                                    <button>
+                                        <span class="material-symbols-outlined">
+                                            send
+                                        </span>
+                                    </button>
+                                </div>
+                                <div class="boxmegusta">
+                                    <p id="megusta">${foto.megusta}</p>
+                                </div>
+                            </div>`;
+    document.body.appendChild(contenedor)
     }
 }
 
-const login = () => {
-    let usuario = document.getElementById("inputusuario").value;
-    let contrasena = document.getElementById("inputcontrasena").value;
-    let contrasenastore = localStorage.getItem(usuario)
-    console.log(contrasenastore)
-    if (contrasena == contrasenastore) {
-        alert("Bienvenido")
-    }else{
-        alert("Usuario o contraseña incorrecta")
-    }
-}
-
-
-
-let contrasena = "";
-let confircontrasena = "";
-
-const vercontrasena = () => {
-    let ojito = document.getElementById("imgojito").src
-    if ( ojito.includes("novisible.svg")) {
-        document.getElementById("imgojito").src ="visible.svg"
-        document.getElementById("inputcontrasena").type = "password"
-    } else {
-        document.getElementById("imgojito").src ="novisible.svg"
-        document.getElementById("inputcontrasena").type = "text"
-    }
-}
-
-let boton = document.getElementById("btnver");
-boton.addEventListener("click", vercontrasena)
-
-const vercontrasena2 = () => {    
-    let ojito2 = document.getElementById("imgojito2").src
-    if ( ojito2.includes("novisible.svg")) {
-        document.getElementById("imgojito2").src ="visible.svg"
-        document.getElementById("inputconfircontrasena").type = "password"
-    } else {
-        document.getElementById("imgojito2").src ="novisible.svg"
-        document.getElementById("inputconfircontrasena").type = "text"
-    }
-}
-
+cargarfotos();
